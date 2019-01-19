77af711772fbbe6457b4d94ed6951ecc8287ded7
Irina Baldwin <2007inb@gmail.com>
2019-01-19 09:09:02 +0000

    Add test to submit a bid

--- cypress/integration/index_spec.js
+++ cypress/integration/index_spec.js
@@ -1,6 +1,8 @@
-describe('My First Test', function() {
-  it('visits the live trading board', function() {
+describe('Live Order Board', function() {
+  before(function() {
     cy.visit('/index.html')
+  })
+  it.skip('has a form for registering an order', function() {
     cy.contains('Live Order Board')
     cy.contains('Enter New Order:')
     cy.contains('User ID')
@@ -8,5 +10,13 @@ describe('My First Test', function() {
     cy.contains('Price')
     cy.contains('Bid')
     cy.contains('Ask')
+  })
+  it('allows user to place a bid', function() {
+    cy.visit('/index.html')
+    cy.get('input[name="userid"]').type('1234')
+    cy.get('input[name="quantity"]').type('100')
+    cy.get('input[name="price"]').type('567')
+    cy.get('input[value="Bid"]').click()
+    cy.get('.orders').should('contain', 'Bid 100 567')
   })
 })
