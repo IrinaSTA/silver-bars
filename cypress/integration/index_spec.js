1850c4154fdc91f487eff91325d36852020abe4a
Irina Baldwin <2007inb@gmail.com>
2019-01-20 21:05:56 +0000

    Add feature test for order summary

--- cypress/integration/index_spec.js
+++ cypress/integration/index_spec.js
@@ -1,8 +1,8 @@
-describe('Live Order Board', function() {
+describe('Live Order Board', function () {
   before(function() {
     cy.visit('/index.html')
   })
-  it('has a form for registering an order', function() {
+  it('has a form for registering an order', function () {
     cy.contains('Live Order Board')
     cy.contains('Enter New Order:')
     cy.contains('User ID')
@@ -11,15 +11,22 @@ describe('Live Order Board', function() {
     cy.contains('Bid')
     cy.contains('Ask')
   })
-  it('allows user to place a bid', function() {
+  it('allows user to place a bid', function () {
     cy.visit('/index.html')
     placeBid()
     cy.get('.orders').should('contain', 'Bid 100 567.00')
   })
-  it('allows user to place an ask', function() {
+  it('allows user to place an ask', function () {
     cy.visit('/index.html')
     placeAsk()
     cy.get('.orders').should('contain', 'Ask 100 567.00')
+  })
+  it('displays order summary', function () {
+    placeBid();
+    placeBid();
+    placeAsk();
+    cy.get('.summary').should('contain', 'Ask 200 567.00')
+    cy.get('.summary').should('contain', 'Bid 200 567.00')
   })
   function placeBid() {
     cy.get('input[name="userid"]').type('1234')
