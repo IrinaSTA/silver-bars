34c0e17774b00b6fe764c730ab53e602312b2297
Irina Baldwin <2007inb@gmail.com>
2019-01-20 17:50:55 +0000

    Isolate orderViewSpec

--- spec/unit/orderViewSpec.js
+++ spec/unit/orderViewSpec.js
@@ -1,28 +1,24 @@
 describe('OrderView', function () {
-  let orderMock;
   let orderView;
 
   beforeEach(function () {
-    orderMock = {
-      userid: '1234',
-      quantity: 100,
-      price: 567.00,
-      type: 'bid',
-    };
-    orderView = new OrderView(orderMock);
+    let order = liveBidMock();
+    orderView = new OrderView(order);
   });
 
   describe('#render', function () {
-    it('returns an html representation of an order', function () {
+    it('returns a formatted orders string', function () {
       const html = orderView.render();
-      expect(html).toEqual("Bid 100 567.00");
+      expect(html).toEqual('Bid 100 567.00');
     });
   });
 
   describe('#renderListItem', function () {
     it('return an html list item with an order', function () {
       const htmlListItem = orderView.renderListItem();
-      expect(htmlListItem).toEqual('<li>Bid 100 567.00</li>');
+      // a list item has nodeType 1
+      expect(htmlListItem.nodeType).toEqual(1);
+      expect(htmlListItem.innerHTML).toEqual('Bid 100 567.00');
     })
   })
 });
